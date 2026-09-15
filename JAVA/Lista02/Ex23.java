2401e74bbaae559b5fad8064bdb180ed36874cec
Arthur Loni <166772542+arthurloni@users.noreply.github.com>
2026-09-15 19:43:34 -0400

    Rename class from Ex12 to Ex23 and update prints

--- JAVA/Lista02/Ex23.java
+++ JAVA/Lista02/Ex23.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Ex12 {
+public class Ex23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -8,10 +8,10 @@ public class Ex12 {
         double Limite;
         double Valor;
 
-        System.out.print("Saldo da conta: R$ ");
+        System.out.print("Saldo da conta: ");
         Saldo = sc.nextDouble();
 
-        System.out.print("Limite diário de saque: R$ ");
+        System.out.print("Limite diário de saque: ");
         Limite = sc.nextDouble();
 
         System.out.print("Valor solicitado: R$ ");
@@ -24,41 +24,8 @@ public class Ex12 {
                 System.out.println("Valor ultrapassa o limite diário");
             } else {
                 Saldo = Saldo - Valor;
-                System.out.printf("Saque efetuado%n");
-                System.out.printf("Novo saldo: R$ %.2f%n", Saldo);
-            }
-        }
-
-        sc.close();
-    }
-}import java.util.Scanner;
-
-public class Ex12 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        double Saldo;
-        double Limite;
-        double Valor;
-
-        System.out.print("Saldo da conta: R$ ");
-        Saldo = sc.nextDouble();
-
-        System.out.print("Limite diário de saque: R$ ");
-        Limite = sc.nextDouble();
-
-        System.out.print("Valor solicitado: R$ ");
-        Valor = sc.nextDouble();
-
-        if (Saldo < Valor) {
-            System.out.println("Saldo insuficiente");
-        } else {
-            if (Valor > Limite) {
-                System.out.println("Valor ultrapassa o limite diário");
-            } else {
-                Saldo = Saldo - Valor;
-                System.out.printf("Saque efetuado%n");
-                System.out.printf("Novo saldo: R$ %.2f%n", Saldo);
+                System.out.printf("Saque efetuado");
+                System.out.printf("Novo saldo: ", Saldo);
             }
         }
 
